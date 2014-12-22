cbaf3d5657fe6a6af4fa017d28b0703e8b462bb0
Piotr Sampławski <sampi60@gmail.com>
2014-12-22 18:53:45 +0100

    e-mail viewer for notifier tests

--- test/mailers/previews/notifier_preview.rb
+++ test/mailers/previews/notifier_preview.rb
@@ -1,4 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notifier
 class NotifierPreview < ActionMailer::Preview
-
+  def new_post
+    sm = Sm.first
+    new_posts = {
+      101 => 'First post',
+      102 => 'Second post',
+      103 => 'Third post',
+      104 => 'Last, but no least post'
+    }
+    Notifier.new_post(sm, new_posts)
+  end
 end
